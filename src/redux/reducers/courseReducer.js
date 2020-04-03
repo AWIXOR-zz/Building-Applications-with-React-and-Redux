196e0d27603ba1c2fd6f4fcdb4b676e0101e2f7f
H.AOUASSAR <hssine36@gmail.com>
2020-04-03 19:01:02 +0100

    actionTypes update

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,6 +1,8 @@
+import * as types from "../actions/actionTypes";
+
 export default function courseResucer(state = [], action) {
   switch (action.type) {
-    case "CREATE_COURSE":
+    case types.CREATE_COURSE:
       return [...state, { ...action.course }];
     default:
       return state;
